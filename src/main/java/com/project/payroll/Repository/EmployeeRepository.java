18f941ec1dc5d40aaff1f6567959bc8cd2019c47
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-18 06:46:07 -0800

    perform CRUD operation

--- src/main/java/com/project/payroll/Repository/EmployeeRepository.java
+++ src/main/java/com/project/payroll/Repository/EmployeeRepository.java
@@ -1,9 +1,15 @@
 package com.project.payroll.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.project.payroll.Entity.Employee;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-    
+
+    // Retrieve Data
+    List<Employee> findByLastName(String lastName);
+    Employee findById(long id);
 }
