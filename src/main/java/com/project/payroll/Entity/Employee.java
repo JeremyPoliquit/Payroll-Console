fdd35a1605d77571a2b53095bd909640ac979390
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-18 06:42:13 -0800

    return to Getter and Setter also adding string format for display data

--- src/main/java/com/project/payroll/Entity/Employee.java
+++ src/main/java/com/project/payroll/Entity/Employee.java
@@ -1,31 +1,60 @@
 package com.project.payroll.Entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Employee {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long emnployeeId;
 
     private String firstName;
     private String lastName;
-    private String sex;
+    private char sex;
 
-    @OneToOne(mappedBy = "employee", cascade = CascadeType.ALL)
-    private Salary salary;
+    public Employee() {
+    };
+
+    @Override
+    public String toString() {
+
+        return String.format(
+            "Employee[id: %d, FirstName: %s, LastName: %s]",
+            emnployeeId, firstName, lastName
+        );
+        
+    }
+
+    public Long getId() {
+        return emnployeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
 
 }
