64140793a0b43eaa76e5a43f81085f4ea871e584
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-18 06:40:50 -0800

    adding retrieving Data

--- src/main/java/com/project/payroll/PayrollApplication.java
+++ src/main/java/com/project/payroll/PayrollApplication.java
@@ -1,87 +1,55 @@
 package com.project.payroll;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
 
 import com.project.payroll.Entity.Employee;
-import com.project.payroll.Entity.Salary;
 import com.project.payroll.Repository.EmployeeRepository;
-
-import jakarta.transaction.Transactional;
 
 import java.util.Scanner;
 
-@SpringBootApplication
-@Component
-public class PayrollApplication implements CommandLineRunner {
 
-	@Autowired
-	private EmployeeRepository employeeRepository;
+@SpringBootApplication
+public class PayrollApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(PayrollApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayrollApplication.class, args);
 	}
 
-	@Transactional
-	@Override
-	public void run(String... args) throws Exception {
-		try {
-
+	@Bean
+	public CommandLineRunner demo(EmployeeRepository repository) {
+		return (args) -> {
 			Employee employee = new Employee();
-			Salary salary = new Salary();
-			Scanner sc = new Scanner(System.in);
 
-			System.out.println("--- Employee Information ---");
-			System.out.print("Employee First Name: ");
-			String firstNameString = sc.nextLine();
-
-			System.out.print("Employee Last Name: ");
-			String lastNameString = sc.nextLine();
-
-			System.out.print("Employee Gender: ");
-			String genderString = sc.nextLine();
-
-
-			
-			System.out.println("--- Employee Salary ---");
-			System.out.print("Salary: ");
-			double salaryDouble = sc.nextDouble();
-
-			System.out.print("Hours Worked: ");
-			int hoursWorkedInteger = sc.nextInt();
+			// Set the data
+			employee.setFirstName("");
+			employee.setLastName("");
+			employee.setSex('');
+	
+			//  Save/Store or Insert into Database
+			repository.save(employee);
+	
+			// Fetch all Data
+			log.info("Employee found with findAll();");
+			log.info("-------------------------------");
+			repository.findAll().forEach(employeeData -> {
+				log.info(employeeData.toString());
+				System.out.println(employeeData);
+			});
 
 
-			employee.setFirstName(firstNameString);
-			employee.setLastName(lastNameString);
-			employee.setSex(genderString);
-			
-			salary.setAmount(salaryDouble);
-			salary.setHoursWorked(hoursWorkedInteger);
-			
-			// Associate Employee with Salary
-			employee.setSalary(salary);
-			salary.setEmployee(employee);
-			
-			// Save to database
-			employeeRepository.save(employee);
-			
-			System.out.println("\n --- Employee Detail --- \n");
-			
-			System.out.println("First Name: " + firstNameString);
-			System.out.println("Last Name: " + lastNameString);
-			System.out.println("Gender: " + genderString + "\n");
-
-			System.out.println("Salary: " + salaryDouble);
-			System.out.println("Hours Worked: " + hoursWorkedInteger);
-			System.out.println("Employee and Salary saved successfully! \n");
-			System.out.println("--- Employee Detail ---");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			// Find by Last Name
+			log.info("Employee find by Lastname ");
+			repository.findByLastName("Palmer").forEach(Palmer -> {
+				log.info(Palmer.toString());
+			});
+		};
 	}
 
 }
